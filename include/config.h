2848478869d45192c076bf5996424eecdd33e0ac
kindanoob <evgpavlov1@gmail.com>
2018-02-16 22:44:49 +0500

    replaced const with constexpr

--- include/config.h
+++ include/config.h
@@ -5,54 +5,54 @@
 #include <vector>
 
 
-const int kWindowWidthInPixels = 1200;
-const int kWindowHeightInPixels = 800;
+constexpr int kWindowWidthInPixels = 1200;
+constexpr int kWindowHeightInPixels = 800;
 
 
 
 const std::string kWIndowTitle = "Game of life";
 
-const int kMapWidthInTiles = 200;
-const int kMapHeightInTiles = 200;
-const int kMapWidthInPixels = kWindowHeightInPixels;
-const int kMapHeightInPixels = kWindowHeightInPixels;
-const int kTileWidthInPixels = kMapWidthInPixels / kMapWidthInTiles;
-const int kTileHeightInPixels = kMapHeightInPixels / kMapHeightInTiles;
-const int kOffsetX = 1 * (kWindowWidthInPixels - kMapWidthInPixels) / 2;
-const int kOffsetY = 1 * (kWindowHeightInPixels - kMapHeightInPixels) / 2;
-const int kMapFillPercentage = 50;
-const int kTurnDelayInMicroseconds = 100000;
-const int kGenerateButtonPressedDelay = 300000;//in microseconds
+constexpr int kMapWidthInTiles = 200;
+constexpr int kMapHeightInTiles = 200;
+constexpr int kMapWidthInPixels = kWindowHeightInPixels;
+constexpr int kMapHeightInPixels = kWindowHeightInPixels;
+constexpr int kTileWidthInPixels = kMapWidthInPixels / kMapWidthInTiles;
+constexpr int kTileHeightInPixels = kMapHeightInPixels / kMapHeightInTiles;
+constexpr int kOffsetX = 1 * (kWindowWidthInPixels - kMapWidthInPixels) / 2;
+constexpr int kOffsetY = 1 * (kWindowHeightInPixels - kMapHeightInPixels) / 2;
+constexpr int kMapFillPercentage = 50;
+constexpr int kTurnDelayInMicroseconds = 100000;
+constexpr int kGenerateButtonPressedDelay = 300000;//in microseconds
 
 const sf::Color kExploredSquareColor = sf::Color(155, 255, 155);
 const sf::Color kGenerateButtonDefaultColor = sf::Color(200, 200, 200);
 
 
 //buttons
-const int kButtonWidth = 120;
-const int kButtonHeight = 50;
+constexpr int kButtonWidth = 120;
+constexpr int kButtonHeight = 50;
 const sf::Vector2f kButtonSize = sf::Vector2f(kButtonWidth, kButtonHeight);
-const int kButtonCharacterSize = 25;
+constexpr int kButtonCharacterSize = 25;
 const sf::Color kButtonTextColor = sf::Color(0, 0, 0);
 const sf::Color kButtonColor = sf::Color(200, 200, 200);
-const int kButtonOutlineThickness = 3;
+constexpr int kButtonOutlineThickness = 3;
 const sf::Color kButtonOutlineColor = sf::Color(0, 0, 0);
 
 
-const int kGenerateButtonPositionX = 50;
-const int kGenerateButtonPositionY = 600;
+constexpr int kGenerateButtonPositionX = 50;
+constexpr int kGenerateButtonPositionY = 600;
 const sf::Vector2f kGenerateButtonPosition = sf::Vector2f(kGenerateButtonPositionX, kGenerateButtonPositionY);
 const std::string kGenerateButtonString = "generate";
 
-const int kSimulateButtonPositionX = 50;
-const int kSimulateButtonPositionY = 600 + kButtonOutlineThickness + kButtonHeight;
+constexpr int kSimulateButtonPositionX = 50;
+constexpr int kSimulateButtonPositionY = 600 + kButtonOutlineThickness + kButtonHeight;
 const sf::Vector2f kSimulateButtonPosition = sf::Vector2f(kSimulateButtonPositionX, kSimulateButtonPositionY);
 const std::string kSimulateButtonString = "simulate";
 
 
 
 
-const double kAlpha = 0.46;// determines the initial spawn region, the closer it is to zero the larger the region is
+constexpr double kAlpha = 0.46;// determines the initial spawn region, the closer it is to zero the larger the region is
 
 const std::vector<int> kBirthValues = {3};// if dead cell has number of neighbors that
                                           // belongs to this set of values, it becomes alive, o/w it stays dead
